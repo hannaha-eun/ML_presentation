1249eeef2766731a4a69328deee55adeac074cd9
hannaha <hannaha@biospyder.com>
2024-05-13 08:10:28 +0100

    app functions done

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import base64
+import io
+
+import pandas as pd
+import numpy as np
+import random
+
 import dash
 from dash import dcc, html, Input, Output, State ,dash_table
-import pandas as pd
+
 import plotly.express as px
+
 # import seaborn as sns
-import io
+## pip install scikit-learn==1.2.2
+import pickle
+from sklearn import metrics
 from sklearn.metrics import confusion_matrix, accuracy_score
-import numpy as np
 
 
 # Initialize the Dash app
@@ -68,7 +76,7 @@ app.layout = html.Div([
             id='balanced-radio',
             options=[
                 {'label': 'Balanced', 'value': 'balanced'},
-                {'label': 'Non-Balanced', 'value': 'non-balanced'}
+                {'label': 'Non-Balanced', 'value': 'random'}
             ],
             value='balanced', className="mb-3"
         ),
@@ -82,11 +90,12 @@ app.layout = html.Div([
                 message='Are you sure you want to start running?'
             ),
         ]),
-        
-        html.Div(id='graph1', className="mb-4"),
-        html.Div(id='graph2', className="mb-4"),
+        dcc.Graph(id="graph1" , className="mb-4"),
+        dcc.Graph(id="graph2" , className="mb-4"),
+        # html.Div(id='graph1', className="mb-4"),
+        # html.Div(id='graph2', className="mb-4"),
    
-        html.Div(id='datatable_test')
+        html.Div(id='datatable_test' , className="mb-4" , style={"color": "black"})
     ], className="container")
 ])
 
@@ -117,45 +126,146 @@ def update_annotation_summary(annotation, filename):
         return '', None
     
     else :
-        summary_text = f"Annotation file '{filename}' uploaded.\n"
+        summary_text = f"Annotation file '{filename}' uploaded.  \n"
         annotation_df=decode_table(annotation)
-        annotation_list=list(set(annotation_df['Annotation'].to_list()))
-        num_samples=str(annotation_df['Sample'].value_counts().shape[0])
-        summary_text += f"Total number of samples: {num_samples}\n"
+        annotation_list=list(set(annotation_df.iloc[:,1].to_list()))
+        # annotation_list=list(set(annotation_df['Annotation'].to_list()))
+        num_samples=str(annotation_df.iloc[:,0].value_counts().shape[0])
+        summary_text += f"Total number of samples: {num_samples}  \n"
         for i in annotation_list : 
-            count=annotation_df['Annotation'].value_counts()[i]
-            summary_text += f"Number of {i}: {count}\n"
+            count=annotation_df.iloc[:,1].value_counts()[i]
+            summary_text += f"Number of {i}: {count}  \n"
     
     return dcc.Markdown(summary_text), None
 
-# Call back function to run calculate accuracy and output plots
+
 @app.callback(
-    [Output('graph1', 'children'),
-     Output('graph2', 'children'),
-    Output('datatable_test', 'children')# this was for testing purpose 
-    ],
-    
-    [Input('upload-model', 'contents'),
-    Input('model-dropdown', 'value'),
-    Input('upload-dataset', 'contents'),
-    Input('upload-annotation', 'contents'),
-     Input('num-samples', 'value'),
-     Input('num_random', 'value'),
-     Input('balanced-radio', 'value')]
+    [Output('graph1', 'figure'),
+     Output('graph2', 'figure'),
+     Output('datatable_test', 'children')],
+    [Input('confirm-start', 'submit_n_clicks')],
+    [State('upload-model', 'contents'),
+     State('model-dropdown', 'value'),
+     State('upload-dataset', 'contents'),
+     State('upload-annotation', 'contents'),
+     State('num-samples', 'value'),
+     State('num_random', 'value'),
+     State('balanced-radio', 'value')]
 )
-def update_output(model, pre_model,count_table,annotation, num_samples, num_random, balance):
-    count_df=decode_table(count_table)
-    annotation_df=decode_table(annotation)
+
+def update_output(submit_n_clicks, model_contents, pre_model, count_table, annotation, num_samples, num_random, balance):
+    if submit_n_clicks is None:
+        return dash.no_update, dash.no_update, dash.no_update
     
-    # Calculate confusion matrix
-    # Calculate accuracy
-    # Create confusion matrix heatmap
-    # Create accuracy histogram
-    return dcc.Graph(), dcc.Graph() ,  dash_table.DataTable(data=count_df.to_dict('records'))
+    # Load machine learning model
+    if model_contents is not None:
+        content_type, content_string = model_contents.split(',')
+        decoded = base64.b64decode(content_string)
+        model = pickle.loads(decoded)
+    elif pre_model is not None:
+        # Load preselected machine learning model
+        model = pickle.load(open(pre_model, "rb"))
+    else:
+        # Handle case when no model is provided
+        return "Error: No model provided.", dash.no_update, dash.no_update
+
+
+
+    n_random=int(num_random)
+    n_sample = int(num_samples)
+
+    count_df = decode_table(count_table)
+    count_df=count_df.set_index(count_df.columns[0])
+    annotation_df = decode_table(annotation)
+    annotation_df=annotation_df.set_index(annotation_df.columns[0])
+    # merge count table and annotation table on sample name 
+    count_df=count_df.T
+    merge_pd=pd.merge(count_df, annotation_df, right_index=True, left_index=True)    
+
+    for index,row in merge_pd.iterrows() : #annot_df
+        annot=row['Class']
+        # this needs to be more sophisicagted for the actual annotation/dataset 
+        ## this is for test purpose -> web app demo purpose 
+        if annot == 'Positive' :
+            merge_pd.loc[index, 'Sample_Class'] = 1
+        else : 
+            merge_pd.loc[index, 'Sample_Class'] = 0
+
+    case_df=merge_pd[merge_pd['Sample_Class'] == 1]
+    control_df=merge_pd[merge_pd['Sample_Class'] == 0]
+
+    accuracy_list=[]
+    accuracy_dict={}
+    accuracy_text = "" 
+    combined_confusion_matrix = np.zeros((2, 2), dtype=int)
+    for randomization in range(n_random):
+        # list of selected sample
+        if balance =='random' :
+            selected_samples = random.sample(list(merge_pd.T.columns), n_sample)
+
+        elif balance =='balanced' : 
+
+            selected_control_samples=random.sample(control_df.index.tolist(), n_sample)
+            selected_case_samples=random.sample(case_df.index.tolist(), n_sample)
+            selected_samples=selected_case_samples + selected_control_samples
+
+        # extract selected sample from count table 
+        randomized_df = merge_pd.loc[selected_samples]
+        randomized_df_sample=randomized_df.drop(columns=['Sample_Class' ,'Class'])
+        # extract selected sample from annotation table 
+        annot_df2 = randomized_df['Sample_Class']
+        
+        # predictuib
+        testing_y_pred = model.predict(randomized_df_sample)
+        # calculate accuracy score 
+        acc = accuracy_score(annot_df2, testing_y_pred)
+        accuracy_list.append(acc)
+        accuracy_dict[randomization+1]=acc
+        
+        print(f"Randomization {randomization + 1}: Accuracy = {acc}  n")
+        accuracy_text += f"Randomization {randomization + 1}: Accuracy = {acc}  \n"
+
+        # plot graphs
+        confusion_matrix = metrics.confusion_matrix(annot_df2, testing_y_pred)
+        combined_confusion_matrix += confusion_matrix
+    print(f"confusion matrix{ combined_confusion_matrix}")
+    acc_df = pd.DataFrame(list(accuracy_dict.items()),columns = ['n_random','accuracy']) 
+    # fig = px.histogram(accuracy_list, range_x=[0, 1], nbins=20)
+
+    counts, bins = np.histogram(accuracy_list,  bins=range(0, 20, 5))
+    bins = 0.5 * (bins[:-1] + bins[1:])
+
+    fig = px.bar(x=bins, y=counts,range_x=[0,1], labels={'x':'n_random', 'y':'accuracy'})
+
+    fig.update_layout(title='Accuracy Distribution',  xaxis=dict(title='Accuracy'), yaxis=dict(title='# of times'))
+
+    # heat_fig=sns.heatmap(combined_confusion_matrix, annot=True, fmt="d", cbar=False) #
+    # heat_fig=px.imshow(combined_confusion_matrix , text_auto=True, aspect="auto") #, text_auto=True, aspect="auto"
+    # heat_fig.update_layout(title='Combined Confusion Matrix',  xaxis=dict(title='Predicted value'), yaxis=dict(title='Real value'))
+
+    heat_fig = px.imshow(combined_confusion_matrix , text_auto=True, aspect="auto" )
+
+    # Customize the layout
+    heat_fig.update_layout(
+        title='Combined Confusion Matrix',
+        xaxis=dict(title='Predicted value'),
+        yaxis=dict(title='Real value'),
+        xaxis_showticklabels=False,
+        yaxis_showticklabels=False, 
+        coloraxis_colorbar=dict(title='Count')  
+    )
+
+    heat_fig.update_traces(hovertemplate='Predicted: %{x}<br>Real: %{y}<br>Count: %{z}<br>%{customdata}')
+
+    return fig, heat_fig  , dash_table.DataTable(data=acc_df.to_dict('records')) #, dcc.Markdown(accuracy_text)
+
+
+
 
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True, port=7777)
+
 
 # upload-model # model-dropdown -> inputs model 
 # upload-dataset -> count table 
